d80b97b8c164f451a712587708c11052e61830b8
KhrisnaAjmadewi <161449546+KhrisnaAjmadewi@users.noreply.github.com>
2024-07-08 19:28:34 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64XPoAaCzq0":
+        Script1();
+        break;
+      case "6KHeXmw1l5W":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  audio.play();
+}
+
+function Script2()
+{
+  toggleMute();
+}
+
